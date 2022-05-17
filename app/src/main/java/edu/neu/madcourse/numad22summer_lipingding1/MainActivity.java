afa3f928f4b3ca695abaa094f129926786855173
liping1120 <ding.li@northeastern.edu>
2022-05-16 21:29:29 -0700

    assignment1-version 1

---
+++ app/src/main/java/edu/neu/madcourse/numad22summer_lipingding1/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad22summer_lipingding1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
