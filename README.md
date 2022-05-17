9381178b77e1a44053ae7fd9f8ddd05d45d03286
studyhard1010 <38797558+studyhard1010@users.noreply.github.com>
2022-05-17 20:30:27 +0800

    assignment1: Fist App.

---
+++ README.md
@@ -0,0 +1,2 @@
+# SV2022SummerCS5520-version3
+mobile app
--- app/src/main/java/edu/neu/madcourse/numad22summer_lipingding1/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22summer_lipingding1/MainActivity.java
@@ -2,7 +2,12 @@ package edu.neu.madcourse.numad22summer_lipingding1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "LipingDing!\nstudyhard1010@gmail.com";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
